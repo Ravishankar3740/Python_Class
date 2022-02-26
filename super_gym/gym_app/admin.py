dfe33e601d360ceedc6a277a72552422c2369701
Ravishankar3740 <ravi.birajdar8880@gmail.com>
2022-02-26 16:10:24 +0530

    import_export

--- super_gym/gym_app/admin.py
+++ super_gym/gym_app/admin.py
@@ -1,4 +1,11 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import Enquiryuser
 # Register your models here.
-admin.site.register(Enquiryuser)
+# admin.site.register(Enquiryuser)
+
+class EnquiryuserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Enquiryuser, EnquiryuserAdmin)
+
--- super_gym/super_gym/settings.py
+++ super_gym/super_gym/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'import_export',
     'gym_app',
     'django.contrib.admin',
     'django.contrib.auth',
